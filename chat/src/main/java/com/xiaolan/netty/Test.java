6175a354d09655339d0998f952bf6a87ec1f4f20
xiaolan-it <xiaolan_it@163.com>
2016-11-01 12:57:15 +0800

    1test

--- chat/src/main/java/com/xiaolan/netty/Test.java
+++ chat/src/main/java/com/xiaolan/netty/Test.java
@@ -13,6 +13,8 @@ import com.xiaolan.netty.v3.MsgVO;
  * @date 2016年10月11日 下午3:11:47
  */
 public class Test {
+	private volatile int m = 0;
+
 	public static void main(String[] args) {
 		MsgVO msgVO = new MsgVO();
 		System.out.println(JSONObject.toJSONString(msgVO));
